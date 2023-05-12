6176ec000ddacd033fcb76ba2fcf89a2cbaca74b
Marcos Alves <marcosalvesjr007@gmail.com>
2023-05-12 23:15:51 +0000

    banco

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('Clientes', 'root', '', {
+const sequelize = new Sequelize('test', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
   });
+
+try {
+    sequelize.authenticate();
+    console.log('Conexão estbabelecida com sucesso')
+}catch{
+    console.log('Erro ao estabelecer conexão');
+}
+
 
   module.exports = {
     Sequelize: Sequelize,
